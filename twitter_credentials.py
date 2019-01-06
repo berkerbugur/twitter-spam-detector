4e7e3908bfc1598008b4339b24752098737f9df6
Berker Bugur <bugur.berker@gmail.com>
2019-01-06 21:20:02 +0300

    Update twitter_credentials.py

--- twitter_credentials.py
+++ twitter_credentials.py
@@ -7,7 +7,7 @@ Created on Tue Dec  4 13:29:35 2018
 #Just holding twitter authentication tokens
 #Twitter uygulaması için gerekli token'ları saklıyoruz
 
-CONSUMER_KEY = '3hIu7AacftJngL23nVNfn91yg'
-CONSUMER_KEY_SECRET = 'xmEMvHt9UeuUOfQEg4JhXlHTh4RZLPwEsoGf9024m4qygyTZe2'
-ACCES_TOKEN = '136378529-Bci3AXssnekal0scbk3eJVqnCgdJ6nHgUFULDNHH'
-ACCES_TOKEN_SECRET = 'BnJl5PqbE01U4ZqO57BRs1xhyAaBAQQeLQ24qvKSoCU4D'
+CONSUMER_KEY = ''
+CONSUMER_KEY_SECRET = ''
+ACCES_TOKEN = ''
+ACCES_TOKEN_SECRET = ''
